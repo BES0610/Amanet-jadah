85c2e2d24493dd2dd4fce7092a66906dae2dbb32
BES0610 <baselsherif9@gmail.com>
2024-10-24 22:26:18 +0300

    edit

--- pages/ReqWaterServiceDoc2/script.js
+++ pages/ReqWaterServiceDoc2/script.js
@@ -1,7 +1,4 @@
     function enableButton() {
         const button = document.getElementById('serviceButton');
         button.disabled = false; // Enable the button when the radio is selected
-        
-        // Scroll to the top of the page
-        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
