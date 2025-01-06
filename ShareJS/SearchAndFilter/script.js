86aacc2a1402d555ca902d48ea97c9eb9419868d
BES0610 <BaselSherif9@gmial.com>
2025-01-06 14:18:24 +0200

    upload

--- ShareJS/SearchAndFilter/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('filter').addEventListener('click', function() {
-    this.classList.toggle('filter-active'); // Change background color
-    const filterInputs = document.getElementById('filterInputs');
-    filterInputs.classList.toggle('hidden'); // Show/hide input fields
-    filterInputs.classList.toggle('filter-active-inputs'); // Toggle flex layout styles
-    // console.log("ss")
-});
